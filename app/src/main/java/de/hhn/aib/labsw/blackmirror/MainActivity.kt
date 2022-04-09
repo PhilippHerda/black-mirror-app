7949782015c3ef394abadc090608fad69e1cf676
Philipp Herda <philippherda@hotmail.com>
2022-04-09 20:06:26 +0200

    here we go again

---
+++ app/src/main/java/de/hhn/aib/labsw/blackmirror/MainActivity.kt
@@ -0,0 +1,20 @@
+package de.hhn.aib.labsw.blackmirror
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val helloBtn = findViewById<Button>(R.id.btn_hello)
+        helloBtn.setOnClickListener {
+            val helloWorldLbl = findViewById<TextView>(R.id.lbl_helloWorld)
+            helloWorldLbl.visibility = View.VISIBLE;
+        }
+    }
+}
