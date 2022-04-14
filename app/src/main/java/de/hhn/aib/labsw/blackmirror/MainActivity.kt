58656cfbeeeedde82af4af166404798569d1aec7
Philipp Herda <philippherda@hotmail.com>
2022-04-14 22:24:12 +0200

    added interface

--- app/src/main/java/de/hhn/aib/labsw/blackmirror/MainActivity.kt
+++ app/src/main/java/de/hhn/aib/labsw/blackmirror/MainActivity.kt
@@ -5,16 +5,48 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import com.fasterxml.jackson.databind.JsonNode
+import de.hhn.aib.labsw.blackmirror.controller.API.MirrorApiListener
+import de.hhn.aib.labsw.blackmirror.controller.API.TopicListener
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TopicListener {
+
+    val mas = MirrorApiListener()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //example for sending
+        val httpClient = OkHttpClient()
+        val request = Request.Builder()
+            .url("ws://10.0.2.2:80")
+            .build()
+
+        val webSocket = httpClient.newWebSocket(request, mas)
+        webSocket.send("{\n" +
+                "  \"topic\":\"location\",\n" +
+                "  \"payload\":{\n" +
+                "    \"lat\":49.12194481375503,\n" +
+                "    \"lon\":9.211276846308193\n" +
+                "  }\n" +
+                "}")
+
+        // subscribe this activity
+        mas.subscribe("location", this)
 
         val helloBtn = findViewById<Button>(R.id.btn_hello)
         helloBtn.setOnClickListener {
             val helloWorldLbl = findViewById<TextView>(R.id.lbl_helloWorld)
             helloWorldLbl.visibility = View.VISIBLE;
         }
+    }
+
+    override fun dataReceived(topic: String?, `object`: JsonNode?) {
+        println("data received")
+        val mapper = mas.getJSONMapper()
+        println(mapper.writeValueAsString(`object`))
     }
 }
---
+++ app/src/main/java/de/hhn/aib/labsw/blackmirror/controller/API/SendPackage.kt
@@ -0,0 +1,8 @@
+package de.hhn.aib.labsw.blackmirror.controller.API
+
+import com.fasterxml.jackson.databind.JsonNode
+
+class SendPackage {
+    var topic: String? = null
+    var payload: JsonNode? = null
+}
