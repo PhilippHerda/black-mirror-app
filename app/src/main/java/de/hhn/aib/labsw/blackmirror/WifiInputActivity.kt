a8a4a0c1f9df9873ec547120ff865d3e9c9a8814
nbinder <nbinder@stud.hs-heilbronn.de>
2022-04-18 17:35:28 +0200

    Wifi SSID input gets checked if it is empty

--- app/src/main/java/de/hhn/aib/labsw/blackmirror/WifiInputActivity.kt
+++ app/src/main/java/de/hhn/aib/labsw/blackmirror/WifiInputActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import java.nio.charset.Charset
 
 class WifiInputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,11 +13,24 @@ class WifiInputActivity : AppCompatActivity() {
         setContentView(R.layout.activity_wifi_input)
 
         val wifiInputButton = findViewById<Button>(R.id.button_ConfirmWifiData)
-        wifiInputButton.setOnClickListener{
+        wifiInputButton.setOnClickListener {
             val wifiSsidEditText = findViewById<EditText>(R.id.editTextText_WifiSSID)
             val wifiPasswordEditText = findViewById<EditText>(R.id.editTextText_WifiPassword)
             println(wifiSsidEditText.text)
             println(wifiPasswordEditText.text)
+            if (wifiSsidEditText.text.toString().isBlank()) {
+                Toast.makeText(
+                    this,
+                    getString(R.string.wifiInput_emptyWifiSSID_toastText),
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                sendWifiData(wifiSsidEditText.text.toString(), wifiPasswordEditText.text.toString())
+            }
         }
+    }
+
+    fun sendWifiData(ssid: String, password: String) {
+        //Schnittstelle
     }
 }
