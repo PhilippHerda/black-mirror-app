e2e8af6bf4883335647a4efd3f8fcf1ead724603
soezdemir <oezdemirselim@yahoo.de>
2022-05-01 21:20:39 +0200

    Drag and Drop implemented

--- app/src/main/java/de/hhn/aib/labsw/blackmirror/WidgetLayoutActivity.kt
+++ app/src/main/java/de/hhn/aib/labsw/blackmirror/WidgetLayoutActivity.kt
@@ -1,21 +1,19 @@
 package de.hhn.aib.labsw.blackmirror
 
+import android.content.ClipData
+import android.os.Build
 import android.os.Bundle
+import android.view.DragEvent
+import android.view.MotionEvent
+import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.Space
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.view.iterator
-import androidx.core.view.marginRight
 import androidx.gridlayout.widget.GridLayout
-import de.hhn.aib.labsw.blackmirror.listener.MyDragListener
-import de.hhn.aib.labsw.blackmirror.listener.MyOnTouchListener
-import java.lang.reflect.Array
 import android.view.ViewGroup
-
-
-
+import androidx.annotation.RequiresApi
 
 
 class WidgetLayoutActivity : AppCompatActivity() {
@@ -35,11 +33,12 @@ class WidgetLayoutActivity : AppCompatActivity() {
         widgets.addAll(listOf("Wetter", "Kalendar"))
         widgetList = findViewById(R.id.widgetList)
 
-        for(i in 0 until widgets.size) {
+        for (i in 0 until widgets.size) {
             val widget = ImageView(this)
             when (widgets[i]) {
                 "Wetter" -> widget.background = AppCompatResources.getDrawable(this, R.drawable.box)
-                "Kalendar" -> widget.background = AppCompatResources.getDrawable(this, R.drawable.box)
+                "Kalendar" -> widget.background =
+                    AppCompatResources.getDrawable(this, R.drawable.box)
             }
             widget.setOnTouchListener(MyOnTouchListener())
             widgetList.addView(widget, i)
@@ -48,8 +47,52 @@ class WidgetLayoutActivity : AppCompatActivity() {
 
     private fun init() {
         myGridLayout = findViewById(R.id.widgetGrid)
-        for(box in myGridLayout) {
+        for (box in myGridLayout) {
             box.setOnDragListener(MyDragListener())
+        }
+    }
+
+    inner class MyDragListener : View.OnDragListener {
+
+        override fun onDrag(v: View?, event: DragEvent?): Boolean {
+            when (event!!.action) {
+                DragEvent.ACTION_DRAG_STARTED -> {}
+                DragEvent.ACTION_DRAG_ENTERED -> {}
+                DragEvent.ACTION_DRAG_EXITED -> {}
+                DragEvent.ACTION_DROP
+                -> {
+                    val view = event?.localState as View
+
+                    when (view.background) {
+                        AppCompatResources.getDrawable(this@WidgetLayoutActivity, R.drawable.box) -> view.background =
+                            AppCompatResources.getDrawable(this@WidgetLayoutActivity, R.drawable.box)
+                        AppCompatResources.getDrawable(this@WidgetLayoutActivity, R.drawable.box) -> view.background =
+                            AppCompatResources.getDrawable(this@WidgetLayoutActivity, R.drawable.box)
+                    }
+                    v!!.background = AppCompatResources.getDrawable(this@WidgetLayoutActivity, R.drawable.selected_box)
+
+                }
+                else -> {}
+            }
+            return true
+        }
+
+        fun checkIfWidgetOnBox(dragEvent: DragEvent): Boolean {
+           return true
+        }
+    }
+
+    inner class MyOnTouchListener : View.OnTouchListener {
+        @RequiresApi(Build.VERSION_CODES.N)
+        override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+            return if (event!!.action == MotionEvent.ACTION_DOWN) {
+                val data = ClipData.newPlainText("", "")
+                val shadowBuilder = View.DragShadowBuilder(v)
+                v!!.startDragAndDrop(data, shadowBuilder, v, 0)
+                true
+            } else {
+                false
+            }
         }
     }
 }
