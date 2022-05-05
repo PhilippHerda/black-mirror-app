0ae45a3337b7643f70453ca5b2f89d568a835992
Luis Gutzeit <lgutzeit@stud.hs-heilbronn.de>
2022-05-05 12:34:09 +0200

    removed/restored files not contained in this branch

--- app/src/main/java/de/hhn/aib/labsw/blackmirror/MainActivity.kt
+++ app/src/main/java/de/hhn/aib/labsw/blackmirror/MainActivity.kt
@@ -1,25 +1,15 @@
 package de.hhn.aib.labsw.blackmirror
 
-import android.content.Intent
-import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import de.hhn.aib.labsw.blackmirror.controller.API.Websockets.MirrorApiWebsockets
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AbstractActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val helloBtn = findViewById<Button>(R.id.btn_hello)
-
-        helloBtn.setOnClickListener {
-            publish("test","test")
-            val intent = Intent(this, WifiInputActivity::class.java)
-            startActivity(intent)
-        }
 
     }
 }
