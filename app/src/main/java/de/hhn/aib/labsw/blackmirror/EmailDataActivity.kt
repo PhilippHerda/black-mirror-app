f1d18e8d76819b7c1d88cc206531c755abb5dc7c
Lukas Michalsky <lmichalsky@stud.hs-heilbronn.de>
2022-05-12 14:30:00 +0200

    added Java Doc

--- app/src/main/java/de/hhn/aib/labsw/blackmirror/EmailDataActivity.kt
+++ app/src/main/java/de/hhn/aib/labsw/blackmirror/EmailDataActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 
+/**
+ * @author Lukas Michalsky
+ * @version 12.05.2022
+ */
 class EmailDataActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,12 +20,12 @@ class EmailDataActivity : AppCompatActivity() {
         val etImap = findViewById<EditText>(R.id.et_imap)
         val etInbox = findViewById<EditText>(R.id.et_inbox)
         val btnSave = findViewById<Button>(R.id.btn_email_save)
+
         btnSave.setOnClickListener {
             var email = etEmail.text.toString()
             var password = etPassword.text.toString()
             var imap = etImap.text.toString()
             var inbox = etInbox.text.toString()
-
         }
     }
 
