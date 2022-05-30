562d5ce893626b826dae067d10e5baba1ba3a892
Lukas Michalsky <lmichalsky@stud.hs-heilbronn.de>
2022-05-30 20:05:06 +0200

    First Version sending EmailData

--- app/src/main/java/de/hhn/aib/labsw/blackmirror/EmailDataActivity.kt
+++ app/src/main/java/de/hhn/aib/labsw/blackmirror/EmailDataActivity.kt
@@ -2,14 +2,16 @@ package de.hhn.aib.labsw.blackmirror
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import de.hhn.aib.labsw.blackmirror.dataclasses.Email
 
 /**
  * @author Lukas Michalsky
- * @version 12.05.2022
+ * @version 30.05.2022
  */
-class EmailDataActivity : AppCompatActivity() {
+class EmailDataActivity : AbstractActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,14 +20,16 @@ class EmailDataActivity : AppCompatActivity() {
         val etEmail = findViewById<EditText>(R.id.et_email)
         val etPassword = findViewById<EditText>(R.id.et_password)
         val etImap = findViewById<EditText>(R.id.et_imap)
-        val etInbox = findViewById<EditText>(R.id.et_inbox)
         val btnSave = findViewById<Button>(R.id.btn_email_save)
 
         btnSave.setOnClickListener {
             var email = etEmail.text.toString()
             var password = etPassword.text.toString()
             var imap = etImap.text.toString()
-            var inbox = etInbox.text.toString()
+
+            Log.i(password, email)
+            val emailData = Email(imap, 993, email, password)
+            publishToRemotes("emailData", emailData)
         }
     }
 
