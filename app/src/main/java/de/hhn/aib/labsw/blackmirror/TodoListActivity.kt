38b5cadeecee055c7841f8671fd0e29b78f99e20
Markus Marewitz <marewitz@stud.hs-heilbronn.de>
2022-05-30 17:58:26 +0200

    fixed list not showing, tested on click listener

--- app/src/main/java/de/hhn/aib/labsw/blackmirror/TodoListActivity.kt
+++ app/src/main/java/de/hhn/aib/labsw/blackmirror/TodoListActivity.kt
@@ -1,8 +1,16 @@
 package de.hhn.aib.labsw.blackmirror
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Build
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import de.hhn.aib.labsw.blackmirror.dataclasses.TodoItem
+import de.hhn.aib.labsw.blackmirror.lists.TodoItemViewHolder
+import de.hhn.aib.labsw.blackmirror.lists.TodoItemsAdapter
+import java.time.ZonedDateTime
 
 class TodoListActivity : AppCompatActivity() {
     private lateinit var todoListView: RecyclerView
@@ -11,9 +19,24 @@ class TodoListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo_list)
 
+        TodoItemViewHolder.onTodoItemClickedCallback = {
+            runOnUiThread {
+                AlertDialog.Builder(this@TodoListActivity)
+                    .setTitle("Todo item clicked!")
+                    .setMessage("'${it.text}'")
+                    .show()
+            }
+        }
+
         todoListView = findViewById(R.id.todoListView)
-        todoListView.adapter = TodoItemsAdapter(listOf(
-            TodoItem("Test todo item", ZonedDateTime.now())
-        ))
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            todoListView.adapter = TodoItemsAdapter(
+                listOf(
+                    TodoItem("Test todo item", ZonedDateTime.now()),
+                    TodoItem("item 2", ZonedDateTime.now()),
+                )
+            )
+        }
     }
 }
--- app/src/main/java/de/hhn/aib/labsw/blackmirror/lists/TodoItemsList.kt
+++ app/src/main/java/de/hhn/aib/labsw/blackmirror/lists/TodoItemsList.kt
@@ -1,6 +1,8 @@
 package de.hhn.aib.labsw.blackmirror.lists
 
+import android.app.Application
 import android.content.Context
+import android.os.Build
 import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
@@ -9,16 +11,34 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import de.hhn.aib.labsw.blackmirror.R
 import de.hhn.aib.labsw.blackmirror.dataclasses.TodoItem
+import java.util.*
 
 class TodoItemViewHolder(
     itemView: View,
     private val context: Context
 ) : RecyclerView.ViewHolder(itemView) {
+    companion object {
+        lateinit var onTodoItemClickedCallback: (TodoItem) -> Unit
+    }
+
     private val dateCreatedTextView = itemView.findViewById<TextView>(R.id.dateCreatedTextView)
     private val todoTextView = itemView.findViewById<TextView>(R.id.todoTextView)
 
+    private lateinit var item: TodoItem
+
+    init {
+        itemView.setOnClickListener {
+            onTodoItemClickedCallback(item)
+        }
+    }
+
     fun bind(item: TodoItem) {
-        dateCreatedTextView.text = DateFormat.getDateFormat(context).format(item.date)
+        this.item = item
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            dateCreatedTextView.text = DateFormat
+                .getDateFormat(context)
+                .format(Date.from(item.date.toInstant()))
+        }
         todoTextView.text = item.text
     }
 }
