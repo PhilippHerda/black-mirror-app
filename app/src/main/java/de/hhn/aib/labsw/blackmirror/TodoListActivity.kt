b96656bd455e4ea355f8c6e7573869013ead6d7e
Markus Marewitz <marewitz@stud.hs-heilbronn.de>
2022-05-31 23:28:33 +0200

    allowing user to add todoItems; now displaying date and time of item creation

--- app/src/main/java/de/hhn/aib/labsw/blackmirror/TodoListActivity.kt
+++ app/src/main/java/de/hhn/aib/labsw/blackmirror/TodoListActivity.kt
@@ -1,15 +1,12 @@
 package de.hhn.aib.labsw.blackmirror
 
-import android.app.Activity
-import android.app.AlertDialog
-import android.app.Dialog
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import de.hhn.aib.labsw.blackmirror.dataclasses.TodoItem
 import de.hhn.aib.labsw.blackmirror.lists.*
 import java.time.ZonedDateTime
@@ -17,8 +14,9 @@ import java.time.ZonedDateTime
 class TodoListActivity : AppCompatActivity() {
     private lateinit var todoListView: RecyclerView
 
-    private lateinit var lastItem: TodoItem
-    private lateinit var lastItemViewHolder: TodoItemViewHolder
+    private var lastClickedItemPos: Int = -1
+
+    private val todoItems = mutableListOf<TodoItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,25 +26,29 @@ class TodoListActivity : AppCompatActivity() {
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
                 if (it.resultCode != RESULT_OK) return@registerForActivityResult
                 val todoText = it.data?.getStringExtra(TODO_TEXT_EXTRA)
-                lastItem.text = todoText ?: return@registerForActivityResult
-                runOnUiThread { lastItemViewHolder.bind(lastItem) }
+                todoItems[lastClickedItemPos].text = todoText ?: return@registerForActivityResult
+                runOnUiThread { todoListView.adapter?.notifyItemChanged(lastClickedItemPos) }
             }
-        TodoItemViewHolder.onTodoItemClickedCallback = { item, viewHolder ->
-            lastItem = item
-            lastItemViewHolder = viewHolder
+        TodoItemViewHolder.onTodoItemClickedCallback = { item, pos ->
+            lastClickedItemPos = pos
             val intent = Intent(this@TodoListActivity, EditTodoActivity::class.java)
             intent.putExtra(TODO_TEXT_EXTRA, item.text)
             activityResultLauncher.launch(intent)
         }
 
         todoListView = findViewById(R.id.todoListView)
+        todoListView.adapter = TodoItemsAdapter(todoItems)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            todoListView.adapter = TodoItemsAdapter(
-                listOf(
-                    TodoItem("Test todo item", ZonedDateTime.now()),
-                    TodoItem("item 2", ZonedDateTime.now()),
-                )
+        val addItemBtn = findViewById<FloatingActionButton>(R.id.addItemButton)
+        addItemBtn.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                lastClickedItemPos = todoItems.size
+                val item = TodoItem("Added item ${todoItems.size + 1}", ZonedDateTime.now())
+                todoItems.add(item)
+            }
+            runOnUiThread { todoListView.adapter?.notifyItemInserted(todoItems.size - 1) }
+            activityResultLauncher.launch(
+                Intent(this@TodoListActivity, EditTodoActivity::class.java)
             )
         }
     }
--- app/src/main/java/de/hhn/aib/labsw/blackmirror/lists/TodoItemsList.kt
+++ app/src/main/java/de/hhn/aib/labsw/blackmirror/lists/TodoItemsList.kt
@@ -11,6 +11,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import de.hhn.aib.labsw.blackmirror.R
 import de.hhn.aib.labsw.blackmirror.dataclasses.TodoItem
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 import java.util.*
 
 class TodoItemViewHolder(
@@ -18,33 +20,34 @@ class TodoItemViewHolder(
     private val context: Context
 ) : RecyclerView.ViewHolder(itemView) {
     companion object {
-        lateinit var onTodoItemClickedCallback: (TodoItem, TodoItemViewHolder) -> Unit
+        lateinit var onTodoItemClickedCallback: (TodoItem, Int) -> Unit
     }
 
     private val dateCreatedTextView = itemView.findViewById<TextView>(R.id.dateCreatedTextView)
     private val todoTextView = itemView.findViewById<TextView>(R.id.todoTextView)
 
     private lateinit var item: TodoItem
+    private var pos: Int = -1
 
     init {
         itemView.setOnClickListener {
-            onTodoItemClickedCallback(item, this)
+            onTodoItemClickedCallback(item, pos)
         }
     }
 
-    fun bind(item: TodoItem) {
+    fun bind(item: TodoItem, position: Int) {
         this.item = item
+        this.pos = position
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            dateCreatedTextView.text = DateFormat
-                .getDateFormat(context)
-                .format(Date.from(item.date.toInstant()))
+            val formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
+            dateCreatedTextView.text = item.date.format(formatter)
         }
         todoTextView.text = item.text
     }
 }
 
 class TodoItemsAdapter(
-    private val items: List<TodoItem>
+    private val items: MutableList<TodoItem>
 ) : RecyclerView.Adapter<TodoItemViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoItemViewHolder {
         return TodoItemViewHolder(LayoutInflater.from(parent.context)
@@ -52,7 +55,7 @@ class TodoItemsAdapter(
     }
 
     override fun onBindViewHolder(viewHolder: TodoItemViewHolder, position: Int) {
-        viewHolder.bind(items[position])
+        viewHolder.bind(items[position], position)
     }
 
     override fun getItemCount() = items.size
