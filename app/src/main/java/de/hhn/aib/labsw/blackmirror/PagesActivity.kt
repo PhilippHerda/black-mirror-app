a6b032a0f579b60cee0943a6ce4fe47081a40c9d
Luis Gutzeit <Luigutzeit@web.de>
2022-06-27 11:38:02 +0200

    added tags save page content

--- app/src/main/java/de/hhn/aib/labsw/blackmirror/PagesActivity.kt
+++ app/src/main/java/de/hhn/aib/labsw/blackmirror/PagesActivity.kt
@@ -111,51 +111,36 @@ class PagesActivity : AppCompatActivity() {
             var pos = 1
             for (box in grid) {
                 if (box.foreground != null) {
-                    when (box.foreground.constantState) {
-                        AppCompatResources.getDrawable(
-                            this,
-                            R.drawable.mail_widget_icon_foreground
-                        )?.constantState -> {
+                    when (box.tag) {
+                        WidgetType.MAIL -> {
                             if (pos % 4 == 0) {
                                 page.widgets.add(Widget(WidgetType.MAIL, 4, pos / 4))
                             } else {
                                 page.widgets.add(Widget(WidgetType.MAIL, pos % 4, pos / 4 + 1))
                             }
                         }
-                        AppCompatResources.getDrawable(
-                            this,
-                            R.drawable.calendar_widget_icon_foreground
-                        )?.constantState -> {
+                        WidgetType.CALENDAR -> {
                             if (pos % 4 == 0) {
                                 page.widgets.add(Widget(WidgetType.CALENDAR, 4, pos / 4))
                             } else {
                                 page.widgets.add(Widget(WidgetType.CALENDAR, pos % 4, pos / 4 + 1))
                             }
                         }
-                        AppCompatResources.getDrawable(
-                            this,
-                            R.drawable.weather_widget_icon_foreground
-                        )?.constantState -> {
+                        WidgetType.WEATHER -> {
                             if (pos % 4 == 0) {
                                 page.widgets.add(Widget(WidgetType.WEATHER, 4, pos / 4))
                             } else {
                                 page.widgets.add(Widget(WidgetType.WEATHER, pos % 4, pos / 4 + 1))
                             }
                         }
-                        AppCompatResources.getDrawable(
-                            this,
-                            R.drawable.clock_widget_icon_foreground
-                        )?.constantState -> {
+                        WidgetType.CLOCK -> {
                             if (pos % 4 == 0) {
                                 page.widgets.add(Widget(WidgetType.CLOCK, 4, pos / 4))
                             } else {
                                 page.widgets.add(Widget(WidgetType.CLOCK, pos % 4, pos / 4 + 1))
                             }
                         }
-                        AppCompatResources.getDrawable(
-                            this,
-                            R.drawable.reminder_widget_icon_foreground
-                        )?.constantState -> {
+                        WidgetType.REMINDER -> {
                             if (pos % 4 == 0) {
                                 page.widgets.add(Widget(WidgetType.REMINDER, 4, pos / 4))
                             } else {
@@ -168,7 +153,7 @@ class PagesActivity : AppCompatActivity() {
             }
             mirror.pages.add(page)
         }
-        if(mirror.pages.isNotEmpty()) {
+        if (mirror.pages.isNotEmpty()) {
             unsavedMirror = mirror
             myMirror = unsavedMirror
             val myToast =
